2b69bd51c9d5b28aeda6461b68ddf6baad00daef
thounny <thounnykeo@gmail.com>
2021-12-07 03:22:46 -0500

    app.route to articles

--- app.js
+++ app.js
@@ -21,6 +21,45 @@ const articleSchema = {
 
 const Article = mongoose.model('Article', articleSchema);
 
+app.route('/article') // Fetches all articles
+
+.get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+})
+
+.post(function(req, res) { // Create new article
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function(err) {
+        if (!err) {
+            res.send("Successfully added a new article");
+        } else {
+            res.send(err);
+        }
+    });
+})
+
+.delete(function(req, res) { // Delete request = deletes all articles within collection
+    Article.deleteMany(function(err) {
+        if (!err) {
+            res.send("Successfully deleted all articles");
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 
 
