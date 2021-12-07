f4acb107fa874bf0ceaea37e4e8d2d335ad60a34
thounny <thounnykeo@gmail.com>
2021-12-07 02:45:01 -0500

    mongoose schema and model setup

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
 
 
 
